c6c3993e6d85410b4d7409c65070c2709ff42812
macroness <macroness@gmail.com>
2018-09-22 19:57:19 +0900

    [20180922] rr() ll() 구현

--- dataStructure/iAVLTree.cpp
+++ dataStructure/iAVLTree.cpp
@@ -18,6 +18,24 @@ int iAVLTree::getBF(const Node& node){
 	return height(node.l) - height(node.r);
 }
 
+iAVLTree::Node* iAVLTree::rr(Node *node) {
+	Node* tempNode = node->r;
+	tempNode = node->r;
+	node->r = tempNode->l;
+	tempNode->l = node;
+
+	return tempNode;
+}
+
+
+iAVLTree::Node* iAVLTree::ll(Node *node) {
+	Node* tempNode = node->l;
+	tempNode = node->l;
+	node->l = tempNode->r;
+	tempNode->r = node;
+
+	return tempNode;
+}
 
 
 
